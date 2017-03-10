583937a177b772d826ffe9290a95942ee18586fe
陳維平 <@WEPIN>
2017-03-10 19:39:20 +0800

    Hello World

--- 0310/Controllers/DefaultController.cs
+++ 0310/Controllers/DefaultController.cs
@@ -11,7 +11,7 @@ namespace _0310.Controllers
         // GET: Default
         public ActionResult Index()
         {
-            ViewBag.Desc = "Hello Worldwewewewwwewewewewe";
+            ViewBag.Desc = "Hello World";
             return View();
         }
     }
